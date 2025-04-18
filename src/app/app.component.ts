4ec54feb142c53f9c26c3ab52827f09ae83fec39
Tushar Singh <tusharsingh5268@gmail.com>
2025-04-19 00:29:58 +0530

    task part added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,29 @@ import { UserComponent } from './user/user.component';
 import { User } from './user/user.model';
 import { DUMMY_USERS } from './dummy-user';
 import { NgFor } from '@angular/common';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,UserComponent,NgFor],
+  imports: [HeaderComponent,UserComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   allUserData:User[]=DUMMY_USERS;
   @ViewChild('list') list!:ElementRef;
-  
-  checkUserSelected(data:boolean){
-    this.list.nativeElement.querySelectorAll('.active').forEach(
-      (question:any) => {
-        question.classList.remove('active');
-      }
-    )
-   
+  seletedUserData!:string;
+  userId!:string
+
+
+  checkUserSelected(id:string){
+    this.userId=id;
+    this.seletedUserData=this.allUserData.find((user)=>{
+      return user.id === id
+    })?.name || ""
   }
+
+  
 }
 
 
